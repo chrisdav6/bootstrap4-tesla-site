693fa544c195063c8f6d8b0139e88d0103817ff5
Chris Davis <chris.davis5440@gmail.com>
2018-03-02 21:08:09 +0000

    Added animations to hero section

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,45 @@
-var scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000
+$(function() {
+  
+  //Initialize Smooth Scroll
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000
+  });
+  
+  //Fade In Animations
+  $("#title").delay(250).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
+  $("#subtitle").delay(350).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
+  $(".col1").delay(450).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
+  $(".col2").delay(550).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+ 
+  $(".col3").delay(650).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
+  $(".col4").delay(750).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
+  $(".col5").delay(850).animate({
+    top: "0",
+    opacity: 1
+  }, 300);
+  
 });
+
