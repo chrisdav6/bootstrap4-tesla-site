1b42d62bac98f382f42fe72263700f6020a256c7
Chris Davis <chris.davis5440@gmail.com>
2018-03-05 15:57:20 +0000

    Completed screen load animations

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,27 +7,27 @@ $(function() {
   
   //Fade In Animations
   $("#title").delay(250).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
   
   $("#subtitle").delay(350).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
   
   $(".col1").delay(450).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
   
   $(".col2").delay(550).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
  
   $(".col3").delay(650).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
   
@@ -37,9 +37,26 @@ $(function() {
   }, 300);
   
   $(".col5").delay(850).animate({
-    top: "0",
+    top: 0,
     opacity: 1
   }, 300);
+  
+  //Fade In video on scroll
+  $(window).scroll(function(){
+    $('.hideme').each( function(i){
+            
+      var top_of_object = $(this).position().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+      if( bottom_of_window > top_of_object ){
+        $(this).delay(300).animate({
+          opacity: 1,
+          top: 0
+        }, 500);
+      }
+            
+    }); 
+  });
   
 });
 
